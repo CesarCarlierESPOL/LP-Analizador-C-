0c3f24a3c1ee22501ed3b69a57f273145bf3366f
CesarCarlierESPOL <ccarlier@espol.edu.ec>
2021-06-27 23:37:31 -0500

    Agregando reglas para iteraciones y condicionales

--- parser.py
+++ parser.py
@@ -1,6 +1,8 @@
 import ply.yacc as yacc
 from lexer import tokens, lexer
 
+success = True
+
 precedence = (
     ("right", "ASSIGN"),
     ("left", "AND", "OR"),
@@ -18,6 +20,24 @@ def p_program(p):
             | empty
     """
 
+def p_external_declaration(p):
+    """
+    external-declaration : type assignment SEMICOLON
+                         | array_usage SEMICOLON
+                         | type array_usage SEMICOLON
+                         | macro_definition
+                         | file_inclusion
+    """
+
+def p_declaration(p):
+    """
+    declaration : type assignment SEMICOLON
+                | assignment SEMICOLON
+                | function_call SEMICOLON
+                | array_usage SEMICOLON
+                | type array_usage SEMICOLON
+    """
+
 def p_assignment(p):
     """
     assignment : ID ASSIGN assignment
@@ -31,8 +51,25 @@ def p_assignment(p):
                | ID TIMES assignment
                | ID DIVIDE assignment
                | ID MODULUS assignment
+               | NUMBER PLUS assignment
+               | NUMBER MINUS assignment
+               | NUMBER TIMES assignment
+               | NUMBER DIVIDE assignment
+               | NUMBER MODULUS assignment
+               | APOST assignment APOST
+               | LPAREN assignment RPAREN
+               | MINUS assignment
+               | NUMBER PLUS PLUS
+               | ID PLUS PLUS
+               | array_usage PLUS PLUS
+               | NUMBER MINUS MINUS
+               | ID MINUS MINUS
+               | array_usage MINUS MINUS
+               | NUMBER
+               | ID
+               | LETTER
     """
-    
+
 def p_function_call(p):
     """
     function_call : ID LPAREN RPAREN
@@ -50,6 +87,60 @@ def p_type(p):
 def p_array_usage(p):
     """
     array_usage : ID LBRACKET assignment RBRACKET
+    """
+
+def p_function(p):
+    """
+    function : type ID LPAREN argument_list_option RPAREN compound_statement
+    argument_list_option : argument_list
+                         | empty
+    argument_list : argument_list COMMA argument
+                  | argument
+    argument : type ID
+    compound_statement : LBRACE statement_list RBRACE
+    statement_list : statement_list statement
+                   | empty
+    statement : iteration_statement
+              | declaration
+              | selection_statement
+              | return-statement
+    """
+
+def p_iteration_statement(p):
+    """
+    iteration_statement : WHILE LPAREN expression RPAREN compound_statement
+                        | WHILE LPAREN expression RPAREN statement
+                        | DO compound_statement WHILE LPAREN expression RPAREN SEMICOLON
+                        | DO statement WHILE LPAREN expression RPAREN SEMICOLON
+    """
+
+def p_selection_statement(p):
+    """
+    selection_statement : IF LPAREN expression RPAREN compound_statement
+                        | IF LPAREN expression RPAREN statement
+                        | IF LPAREN expression RPAREN compound_statement ELSE compound_statement
+                        | IF LPAREN expression RPAREN compound_statement ELSE statement
+                        | IF LPAREN expression RPAREN statement ELSE compound_statement
+                        | IF LPAREN expression RPAREN statement ELSE statement
+    """
+
+def p_return_statement(p):
+    """
+    return-statement : RETURN SEMICOLON
+                     | RETURN expression SEMICOLON
+    """
+
+
+def p_expression(p):
+    """
+    expression : expression EQUALS expression
+               | expression LESS expression
+               | expression GREATER expression
+               | expression AND expression
+               | expression OR expression
+               | NOT expression
+               | assignment
+               | array_usage
     """
 
 def p_macro_definition(p):
@@ -83,9 +174,11 @@ parser = yacc.yacc()
 source_code = """
 # include <stdlib.h>
 # define TRUE 1
-// comment
+
 int main (int a) {
+    int b= 0;
     if (a =  2) {
+        b = 3;
     }
 }
 """
@@ -105,3 +198,8 @@ lexer.lineno = 0
 
 # Parse
 parser.parse(source_code)
+
+if success:
+    print("Código Válido")
+else:
+    print("Código no Válido")
