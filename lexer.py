a67e1e66ebe5694061a8900947df01100b94f3e7
CesarCarlierESPOL <ccarlier@espol.edu.ec>
2021-06-27 20:42:43 -0500

    Cambio al Lexer

--- lexer.py
+++ lexer.py
@@ -1,183 +1,134 @@
 import ply.lex as lex
 
 # lista de tokens
-tokens = (
-    'NUMBERS', 'FLOAT',
-    'OPMAS', 'OPMENOS', 'OPMULTIPLICAR', 'OPDIVISOR', 'OPMODULE', 'OPIGUAL',
-    'LPAREN', 'RPAREN', 'SEMICOLON', 'CADENA'
-)
 
+#Cesar: Aumente tokens faltantes
+tokens = [
+    "NUMBER",
+    "LETTER",
+    "PLUS",
+    "MINUS",
+    "TIMES",
+    "DIVIDE",
+    "MODULUS",
+    "AND",
+    "OR",
+    "NOT",
+    "EQUALS",
+    "LESS",
+    "GREATER",
+    "LPAREN",
+    "RPAREN",
+    "HEADER",
+    "ID",
+    "COMMA",
+    "SEMICOLON",
+    "APOST",
+    "QUOTE",
+    "LBRACE",
+    "RBRACE",
+    "LBRACKET",
+    "RBRACKET",
+    "POUND",
+    "COMMENT",
+    "COMMENTBLOCK",
+    "ASSIGN",
+    "EOF"
+]
 
+#Cesar: Reduje numero de palabras reservadas, utilizabamos toda la libreria de C++, que no ibamos a cubrir
 reserved = {
-    'while': 'WHILE',
-    'else': 'ELSE',
-    'if': 'IF',
-    'for': 'FOR',
-    'switch': 'SWITCH',
-    'case': 'CASE',
-    'do': 'DO',
-    'break': 'BREAK',
-    'return': 'RETURN',
-    'int': 'INT',
-    'float': 'FLOAT',
-    'double': 'DOUBLE',
-    'continue': 'CONTINUE',
-    'struct': 'STRUCT',
-    'union': 'UNION',
-    'char': 'CHAR',
-    'alignas': 'ALIGNAS',
-    'alignof': 'ALIGNOF',
-    'and': 'AND',
-    'and_eq': 'AND_EQ',
-    'asm': 'ASM',
-    '__abstract': '__ABSTRACT',
-    'delete': 'DELETE',
-    'this': 'THIS',
-    'auto': 'AUTO',
-    'bitand': 'BITAND',
-    'constinit': 'CONSTINIT',
-    'continue': 'CONTINUE',
-    'co_await': 'CO_AWAIT',
-    'co_return': 'CO_RETURN',
-    'co_yield': 'CO_YIELD',
-    'decltype': 'DECLTYPE',
-    'default': 'DEFAULT',
-    'long': 'LONG',
-    'mutable': 'MUTABLE',
-    'namespace': 'NAMESPACE',
-    'new': 'NEW',
-    'noexcept': 'NOEXCEPT',
-    'not': 'NOT',
-    'static_assert': 'STATIC_ASSERT',
-    'static_cast': 'STATIC_CAST',
-    'struct': 'STRUCT',
-    'switch': 'SWITCH',
-    'template': 'TEMPLATE',
-    'this': 'THIS',
-    'dinamic_cast': 'DINAMIC_CAST',
-    'const_cast': 'CONST_CAST',
-    'new': 'NEW',
-    'return': 'RETURN',
-    'noreturn': 'NORETURN',
-    'thread_local': 'THREAD_LOCAL',
-    'bitor': 'BITOR',
-    'bool': 'BOOL',
-    'catch': 'CATCH',
-    'char8_t': 'CHAR8_T',
-    'char16_t': 'CHAR16_T',
-    'char32_t': 'CHAR32_T',
-    'class': 'CLASS',
-    'compl_b': 'COMPL_B',
-    'concept': 'CONCEPT',
-    'const': 'CONST',
-    'const_cast': 'CONST_CAST',
-    'consteval': 'CONSTEVAL',
-    'constexpr': 'CONTEXPR',
-    'delete': 'DELETE',
-    'do': 'DO',
-    'dynamic_cast': 'DYNAMIC_CAST',
-    'enum': 'ENUM',
-    'explicit': 'EXPLICIT',
-    'export': 'EXPORT',
-    'extern': 'EXTERN',
-    'false': 'FALSE',
-    'goto': 'GOTO',
-    'inline': 'INLINE',
-    'not_eq': 'NOT_EQ',
-    'nullptr': 'NULLPTR',
-    'operator': 'OPERATOR',
-    'or_eq': 'OR_EQ',
-    'private': 'PRIVATE',
-    'protected': 'PROTECTED',
-    'public': 'PUBLIC',
-    'register': 'REGISTER',
-    'reinterpret_cast': 'REINTERPRET_CAST',
-    'requires': 'REQUIRES',
-    'short': 'SHORT',
-    'signed': 'SIGNED',
-    'sizeof': 'SIZEOF',
-    'static': 'STATIC',
-    'throw': 'THROW',
-    'try': 'TRY',
-    'typedef': 'TYPEDEF',
-    'typeid': 'TYPEID',
-    'typename': 'TYPENAME',
-    'union': 'UNION',
-    'unsigned': 'UNSIGNED',
-    'using': 'USING',
-    'virtual': 'VIRTUAL',
-    'void': 'VOID',
-    'volatile': 'VOLATILE',
-    'wchar_t': 'wchar_t',
-    'xor': 'XOR',
-    'xor_eq': 'XOR_EQ',
-    '!': '!',
-    '#': '#',
-    '%': '%',
-
+    "int": "INT",
+    "char": "CHAR",
+    "float": "FLOAT",
+    "if": "IF",
+    "else": "ELSE",
+    "do": "DO",
+    "while": "WHILE",
+    "return": "RETURN",
+    "void": "VOID",
+    "define": "DEFINE",
+    "include": "INCLUDE"
 }
-# Regular expression rules for simple tokens
-t_OPMAS = r'\+'
-t_OPMENOS = r'-'
-t_OPMULTIPLICAR = r'\*'
-t_OPDIVISOR = r'/'
-t_OPIGUAL = r'='
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
-t_SEMICOLON = r';'
-t_OPMODULE = r'%'
+
+tokens += list(reserved.values())
+
+#Cesar: Ajustando la definicion de tokens para el cambio.
+t_PLUS = r"\+"
+t_MINUS = r"\-"
+t_TIMES = r"\*"
+t_DIVIDE = r"\/"
+t_MODULUS = r"\%"
+t_AND = r"\&\&"
+t_OR = r"\|\|"
+t_NOT = r"\!"
+t_EQUALS = r"\=\="
+t_LESS = r"\<"
+t_GREATER = r"\>"
+t_LPAREN = r"\("
+t_RPAREN = r"\)"
+t_COMMA = r"\,"
+t_SEMICOLON = r"\;"
+t_APOST = r"\'"
+t_QUOTE = r"\""
+t_LBRACE = r"\{"
+t_RBRACE = r"\}"
+t_LBRACKET = r"\["
+t_RBRACKET = r"\]"
+t_POUND = r"\#"
+t_ASSIGN = r"\="
+t_EOF = r"\$"
 
 
-def t_NUMBERS(t):
-    r'\d+'
+#Cesar modifique para que esto funcione con los nuevos nombres
+def t_COMMENT(t):
+    r"\/\/.*"
+    return t
+
+
+def t_COMMENTBLOCK(t):
+    r"\/\*(.|\n)*\*\/"
+    return t
+
+
+def t_HEADER(t):
+    r"[a-zA-Z_][a-zA-Z_0-9]*\.h"
+    return t
+
+
+def t_ID(t):
+    r"[a-zA-Z_][a-zA-Z_0-9]*"
+    t.type = reserved.get(t.value, "ID")  # Check for reserved words
+    return t
+
+
+def t_NUMBER(t):
+    r"\d+(\.\d+)?"
     t.value = int(t.value)
     return t
 
 
-def t_FLOAT(t):
-    r'(\d*\.\d+)|(\d+\.\d*)'
-    t.value = float(t.value)
+def t_LETTER(t):
+    r"\'.\'"
+    t.value = t.value.replace("'", "")
     return t
 
 
-def t_CADENA(t):
-    r'[A-z]+'
-    t.value = str(t.value)
-    return t
-
-# Define a rule so we can track line numbers
-
-
+# Cesar: Regla para definir los numeros de linea
 def t_newline(t):
-    r'\n+'
+    r"\n+"
     t.lexer.lineno += len(t.value)
 
 
-# A string containing ignored characters (spaces and tabs)
-t_ignore = ' \t'
+# Regla para espacios y tabs
+t_ignore = " \t"
 
-
-# Error handling rule
+# Regla para errores
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
-
-# Build the lexer
 lexer = lex.lex()
 
-# Test it out
-data = '''
-23+2.5;
-hola;
- '''
-
-# Give the lexer some input
-lexer.input(data)
-
-# Tokenize
-while True:
-    tok = lexer.token()
-    if not tok:
-        break  # No more input
-    print(tok)
+def tokenize(data):
+    lexer.input(data)
